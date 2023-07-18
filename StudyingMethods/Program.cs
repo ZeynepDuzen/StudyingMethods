8113e9e92e4df007114ddedc72583074f275a254
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-18 21:54:19 +0300

    Proje dosyası ekle.

---
+++ StudyingMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingMethods
+{
+    internal class Program
+    {
+        private static void Okul() {
+
+            Console.WriteLine("Okul adi : Bilisim Lisesi");
+            Console.WriteLine("Okul Muduru : Serhat Can");
+            Console.WriteLine("Ogretmen : Zeynep Cankus");
+            Console.WriteLine("Sehir : Bursa");
+
+        }
+        static void Main(string[] args)
+        {
+            Okul();
+            Console.Read();
+        }
+    }
+}
